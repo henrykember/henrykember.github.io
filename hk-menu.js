945a1af2dee2dfe0d2b3a7d5a886237eca2386a5
henrykember <hmkember@gmail.com>
2016-09-09 12:57:19 -0700

    Trying a different loader

--- hk-menu.js
+++ hk-menu.js
@@ -2,69 +2,87 @@
 
 
 $(document).ready(function() {
-	$('#preloader').fadeOut('slow',function(){$(this).remove();});
-		startup();
+	// $('#preloader').fadeOut('slow',function(){$(this).remove();});
+	// 	startup();
 	// function turnoffpreloader(){
 		
 	// }
 
 	// setTimeout(turnoffpreloader, 2000);
 
+ var minimumShowLoadingTimeReached = false;
+    setTimeout(function() {minimumShowLoadingTimeReached = true;}, 3000);
+
+    function preloader() {
+      if (minimumShowLoadingTimeReached) {
+          showContent();
+      } else {
+          setTimeout(function() {
+            preloader();
+          }, 500);
+      }
+    }
+
+    function showContent(){
+        document.getElementById("loading").style.display = "none";
+        document.getElementById("content").style.display = "block";
+    }
+    window.onload = preloader;
 });
 
-var startup = function ()  {
-	var fivetonine = $("#fivetonine");
-	var ninetofive = $("#ninetofive");
-	var all = $("#all");
-	var business = $(".business");
-	var casual = $(".casual");
-		// console.log("hi");
+// var startup = function ()  {
+// 	var fivetonine = $("#fivetonine");
+// 	var ninetofive = $("#ninetofive");
+// 	var all = $("#all");
+// 	var business = $(".business");
+// 	var casual = $(".casual");
+// 		// console.log("hi");
 
-	fivetonine.on('click', function ()  {	
-	console.log("hi");	
-		business.hide(200);
-		casual.show(200);
-		// casual.toggle(200);
-	});
+// 	fivetonine.on('click', function ()  {	
+// 	console.log("hi");	
+// 		business.hide(200);
+// 		casual.show(200);
+// 		// casual.toggle(200);
+// 	});
 
-	fivetonine.off('click', function ()  {
-		console.log("hi");	
-		business.show(200);
-		casual.show(200);
-		// casual.toggle(200);
-	});
+// 	fivetonine.off('click', function ()  {
+// 		console.log("hi");	
+// 		business.show(200);
+// 		casual.show(200);
+// 		// casual.toggle(200);
+// 	});
 	
-	ninetofive.on('click', function ()  {
-		// console.log("hi");
-		// casual.toggle(200);
-		// business.toggle(200);
-		business.show(200);
-		casual.hide(200);
-	});
+// 	ninetofive.on('click', function ()  {
+// 		// console.log("hi");
+// 		// casual.toggle(200);
+// 		// business.toggle(200);
+// 		business.show(200);
+// 		casual.hide(200);
+// 	});
 
-	ninetofive.off('click', function ()  {
-		// console.log("hi");
-		// casual.toggle(200);
-		// business.toggle(200);
-		business.show(200);
-		casual.show(200);
-	});
+// 	ninetofive.off('click', function ()  {
+// 		// console.log("hi");
+// 		// casual.toggle(200);
+// 		// business.toggle(200);
+// 		business.show(200);
+// 		casual.show(200);
+// 	});
 
-	all.on('click', function ()  {
-		// console.log("hi");
-		// casual.toggle(200);
-		// business.toggle(200);
-		business.show(200);
-		casual.show(200);
-	});
+// 	all.on('click', function ()  {
+// 		// console.log("hi");
+// 		// casual.toggle(200);
+// 		// business.toggle(200);
+// 		business.show(200);
+// 		casual.show(200);
+// 	});
 
-	all.off('click', function ()  {
-		// console.log("hi");
-		// casual.toggle(200);
-		// business.toggle(200);
-		business.show(200);
-		casual.show(200);
-	});
+// 	all.off('click', function ()  {
+// 		// console.log("hi");
+// 		// casual.toggle(200);
+// 		// business.toggle(200);
+// 		business.show(200);
+// 		casual.show(200);
+// 	});
 };
 
 
