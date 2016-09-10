0664670de97721da7980e84c52201621f12788d3
henrykember <hmkember@gmail.com>
2016-09-09 23:40:49 -0700

    Turned selector js back on

--- hk-menu.js
+++ hk-menu.js
@@ -39,60 +39,60 @@ $(document).ready(function() {
 
 
 
-// var startup = function ()  {
-// 	var fivetonine = $("#fivetonine");
-// 	var ninetofive = $("#ninetofive");
-// 	var all = $("#all");
-// 	var business = $(".business");
-// 	var casual = $(".casual");
-// 		// console.log("hi");
+var startup = function ()  {
+	var fivetonine = $("#fivetonine");
+	var ninetofive = $("#ninetofive");
+	var all = $("#all");
+	var business = $(".business");
+	var casual = $(".casual");
+		// console.log("hi");
 
-// 	fivetonine.on('click', function ()  {	
-// 	console.log("hi");	
-// 		business.hide(200);
-// 		casual.show(200);
-// 		// casual.toggle(200);
-// 	});
+	fivetonine.on('click', function ()  {	
+	console.log("hi");	
+		business.hide(200);
+		casual.show(200);
+		// casual.toggle(200);
+	});
 
-// 	fivetonine.off('click', function ()  {
-// 		console.log("hi");	
-// 		business.show(200);
-// 		casual.show(200);
-// 		// casual.toggle(200);
-// 	});
+	fivetonine.off('click', function ()  {
+		console.log("hi");	
+		business.show(200);
+		casual.show(200);
+		// casual.toggle(200);
+	});
 	
-// 	ninetofive.on('click', function ()  {
-// 		// console.log("hi");
-// 		// casual.toggle(200);
-// 		// business.toggle(200);
-// 		business.show(200);
-// 		casual.hide(200);
-// 	});
+	ninetofive.on('click', function ()  {
+		// console.log("hi");
+		// casual.toggle(200);
+		// business.toggle(200);
+		business.show(200);
+		casual.hide(200);
+	});
 
-// 	ninetofive.off('click', function ()  {
-// 		// console.log("hi");
-// 		// casual.toggle(200);
-// 		// business.toggle(200);
-// 		business.show(200);
-// 		casual.show(200);
-// 	});
+	ninetofive.off('click', function ()  {
+		// console.log("hi");
+		// casual.toggle(200);
+		// business.toggle(200);
+		business.show(200);
+		casual.show(200);
+	});
 
-// 	all.on('click', function ()  {
-// 		// console.log("hi");
-// 		// casual.toggle(200);
-// 		// business.toggle(200);
-// 		business.show(200);
-// 		casual.show(200);
-// 	});
+	all.on('click', function ()  {
+		// console.log("hi");
+		// casual.toggle(200);
+		// business.toggle(200);
+		business.show(200);
+		casual.show(200);
+	});
 
-// 	all.off('click', function ()  {
-// 		// console.log("hi");
-// 		// casual.toggle(200);
-// 		// business.toggle(200);
-// 		business.show(200);
-// 		casual.show(200);
-// 	});
-//};
+	all.off('click', function ()  {
+		// console.log("hi");
+		// casual.toggle(200);
+		// business.toggle(200);
+		business.show(200);
+		casual.show(200);
+	});
+};
 
 
 
