7f4839a5407467ae44819642fa60ae853e821fb9
henrykember <hmkember@gmail.com>
2020-09-30 19:03:17 -0400

    inverted play/pause

--- hk-menu5autoplay.js
+++ hk-menu5autoplay.js
@@ -75,14 +75,24 @@ $(document).ready(function() {
 	
 
 
+// var figure = $(".playvid").hover( hoverVideo, hideVideo );
+
+// function hoverVideo(e) {  
+//     $('video', this).get(0).play(); 
+// }
+
+// function hideVideo(e) {
+//     $('video', this).get(0).pause(); 
+// }
+
 var figure = $(".playvid").hover( hoverVideo, hideVideo );
 
 function hoverVideo(e) {  
-    $('video', this).get(0).play(); 
+    $('video', this).get(0).pause(); 
 }
 
 function hideVideo(e) {
-    $('video', this).get(0).pause(); 
+    $('video', this).get(0).play(); 
 }
 
 });
