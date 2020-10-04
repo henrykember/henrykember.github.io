9250abb3913f1fd085dd95cc582952e840955484
henrykember <hmkember@gmail.com>
2020-10-04 00:32:44 -0400

    Columns, vw calcs for new images

--- hk-menu5autoplay.js
+++ hk-menu5autoplay.js
@@ -88,11 +88,11 @@ $(document).ready(function() {
 var figure = $(".playvid").hover( hoverVideo, hideVideo );
 
 function hoverVideo(e) {  
-    $('video', this).get(0).pause(); 
+    $('video', this).get(0).play(); 
 }
 
 function hideVideo(e) {
-    $('video', this).get(0).play(); 
+    $('video', this).get(0).pause(); 
 }
 
 });
