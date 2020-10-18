a88d0d0c736ee75135099a820f5962cbdc6ba5ef
henrykember <hmkember@gmail.com>
2020-10-18 10:46:15 -0400

    Building out V7 px menu

--- hk-menu8wip.js
+++ hk-menu8wip.js
@@ -52,6 +52,15 @@ function hideVideo(e) {
     $('video', this).get(0).pause(); 
 }
 
+function myFunction(x) {
+  if (x.matches) { 
+    $("#telstrawrapper").detach().appendTo(".one");
+  }
+}
+
+var x = window.matchMedia("(max-width: 1024px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
 
 });
 
